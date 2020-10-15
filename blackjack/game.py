2e56917464e235ebfc6ad0f2bb1d7ca03bdaf84e
takuodoi <doi@level-five.jp>
2020-10-16 08:31:18 +0900

    Make filels private.

--- blackjack/game.py
+++ blackjack/game.py
@@ -13,66 +13,69 @@ class GameIsNotFinishedException(Exception):
 class Game:
 
     def __init__(self):
-        self.initialized = False
-        self.finished = False
-        self.deck = None
-        self.players = []
-        self.hands = dict()
-        self.dealer_hand = Hand()
+        self.__initialized = False
+        self.__finished = False
+        self.__deck = None
+        self.__players = []
+        self.__hands = dict()
+        self.__dealer_hand = Hand()
 
     def is_initialized(self):
-        return self.initialized
+        return self.__initialized
 
     def setup(self, deck, players):
-        self.initialized = True
-        self.deck = deck
+        self.__initialized = True
+        self.__deck = deck
         for player in players:
-            self.players.append(player)
+            self.__players.append(player)
             hand = Hand()
-            hand.add(self.deck.next())
-            hand.add(self.deck.next())
-            self.hands[player] = hand
-        self.dealer_hand.add(self.deck.next())
-        self.dealer_hand.add(self.deck.next())
+            hand.add(self.__deck.next())
+            hand.add(self.__deck.next())
+            self.__hands[player] = hand
+        self.__dealer_hand.add(self.__deck.next())
+        self.__dealer_hand.add(self.__deck.next())
 
     def play(self):
-        for player in self.players:
+        for player in self.__players:
             while player.action(self) == Action.HIT:
-                self.hands[player].add(self.deck.next())
+                self.__hands[player].add(self.__deck.next())
 
-        while self.dealer_hand.score() < 17:
-            self.dealer_hand.add(self.deck.next())
+        while self.__dealer_hand.score() < 17:
+            self.__dealer_hand.add(self.__deck.next())
 
-        self.finished = True
+        self.__finished = True
+
+    def get_players(self):
+        return self.__players
 
     def get_player_hand(self, player):
-        if not self.initialized:
+        if not self.__initialized:
             raise GameIsNotInitializedException()
-        return self.hands[player]
+        return self.__hands[player]
 
     def get_dealer_up_card(self):
-        if not self.initialized:
+        if not self.__initialized:
             raise GameIsNotInitializedException()
-        return self.dealer_hand.get(0)
+        return self.__dealer_hand.get(0)
 
     def get_dealer_hand(self):
-        if not self.initialized:
+        if not self.__initialized:
             raise GameIsNotInitializedException()
 
-        if not self.finished:
+        if not self.__finished:
             raise GameIsNotFinishedException()
 
-        return self.dealer_hand
+        return self.__dealer_hand
 
     def show_result(self):
         print("Dealer", end=",")
-        for card in self.dealer_hand.cards:
+        for card in self.__dealer_hand.cards:
             print(card, end=",")
         print("")
-        for player in self.players:
+        for player in self.__players:
             hand = self.get_player_hand(player)
             print(player.get_name(), end=",")
-            print(Hand.check(hand, self.dealer_hand), end=",")
+            print(Hand.check(hand, self.__dealer_hand), end=",")
             for card in hand.cards:
                 print(card, end=",")
             print("")
